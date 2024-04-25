9328be87b2cae4dc57237934c56b06a13b3e6f00
pederhan <pederhan@usit.uio.no>
2024-04-25 12:33:23 +0200

    Remove unused inspect code

--- zabbix_auto_config/hostmodifiers/legacy.py
+++ zabbix_auto_config/hostmodifiers/legacy.py
@@ -5,27 +5,12 @@ Used to provide backwards compatibility with older versions of ZAC.
 
 from __future__ import annotations
 
-import inspect
-import logging
-from functools import lru_cache
-from typing import Any
-from typing import Callable
 from typing import Protocol
 from typing import runtime_checkable
 
 from zabbix_auto_config.hostmodifiers.base import BaseModifier
 from zabbix_auto_config.models import Host
 from zabbix_auto_config.models import Settings
-
-
-@lru_cache(maxsize=None)
-def takes_kwargs(func: Callable[..., Any]) -> bool:
-    """Returns True if the given function accepts keyword arguments."""
-    logging.debug("We are in takes_kwargs")
-    return any(
-        p.kind == inspect.Parameter.VAR_KEYWORD
-        for p in inspect.signature(func).parameters.values()
-    )
 
 
 @runtime_checkable
